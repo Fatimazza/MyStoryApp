24f50b04702aedbe51ef4fa63df169d15e613182
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-10 23:32:47 +0700

    Login - Implement View Binding

--- app/src/main/java/xyz/codingwithza/mystoryapp/view/login/LoginActivity.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/view/login/LoginActivity.kt
@@ -2,11 +2,15 @@ package xyz.codingwithza.mystoryapp.view.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import xyz.codingwithza.mystoryapp.R
+import xyz.codingwithza.mystoryapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
