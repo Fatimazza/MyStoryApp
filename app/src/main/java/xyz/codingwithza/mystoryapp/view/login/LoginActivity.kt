10b53be0ee0e0055a6c86e8053e00e502d5c2240
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-10 18:18:49 +0700

    Login - Add new activity

---
+++ app/src/main/java/xyz/codingwithza/mystoryapp/view/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package xyz.codingwithza.mystoryapp.view.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import xyz.codingwithza.mystoryapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
