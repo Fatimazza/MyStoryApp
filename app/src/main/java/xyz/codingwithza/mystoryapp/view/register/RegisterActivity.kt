822f3765d4048122dd1eac49523485e4b2b0fb38
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-10 23:34:28 +0700

    Register - Implement View Binding

--- app/src/main/java/xyz/codingwithza/mystoryapp/view/register/RegisterActivity.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/view/register/RegisterActivity.kt
@@ -2,11 +2,15 @@ package xyz.codingwithza.mystoryapp.view.register
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import xyz.codingwithza.mystoryapp.R
+import xyz.codingwithza.mystoryapp.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
