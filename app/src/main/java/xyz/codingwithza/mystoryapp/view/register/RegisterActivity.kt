0d6cc62ca6dd4ee73d819470d3cbf2167a2b5c1d
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-10 17:38:31 +0700

    Register - Add new activity

---
+++ app/src/main/java/xyz/codingwithza/mystoryapp/view/register/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package xyz.codingwithza.mystoryapp.view.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
