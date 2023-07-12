07ab3ddae77d37573cdaeb68d63fbdddb2396296
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-12 17:28:20 +0700

    Register - Add Register API Service

--- app/src/main/java/xyz/codingwithza/mystoryapp/network/ApiService.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/network/ApiService.kt
@@ -1,5 +1,17 @@
 package xyz.codingwithza.mystoryapp.network
 
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+import xyz.codingwithza.mystoryapp.model.RegisterResponse
+
 interface ApiService {
 
+    @FormUrlEncoded
+    @POST("register")
+    suspend fun register(
+        @Field("name") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): RegisterResponse
 }
