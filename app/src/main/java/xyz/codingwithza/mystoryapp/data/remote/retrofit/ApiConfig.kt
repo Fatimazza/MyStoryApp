fe0a1cf7100f737c83baec3ac39f17d1bc9c72af
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-24 13:46:24 +0700

    Network - Fix Retrofit configuration in API Config

--- app/src/main/java/xyz/codingwithza/mystoryapp/data/remote/retrofit/ApiConfig.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/data/remote/retrofit/ApiConfig.kt
@@ -6,19 +6,19 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import xyz.codingwithza.mystoryapp.BuildConfig
 
-class ApiConfig {
-    fun getApiService(): ApiService {
-        val loggingInterceptor =
-            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-        val client = OkHttpClient.Builder()
-            .addInterceptor(loggingInterceptor)
-            .build()
-        
-        val retrofit = Retrofit.Builder()
-            .baseUrl(BuildConfig.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(client)
-            .build()
-        return retrofit.create(ApiService::class.java)
-    }
+object ApiConfig {
+        fun getApiService(): ApiService {
+            val loggingInterceptor =
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BuildConfig.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+            return retrofit.create(ApiService::class.java)
+        }
 }
