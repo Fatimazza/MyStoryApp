28946b967f70d43d28d46371621e70db5b7ba740
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-24 14:25:55 +0700

    Register - Init View Model in Activity

--- app/src/main/java/xyz/codingwithza/mystoryapp/view/register/RegisterActivity.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/view/register/RegisterActivity.kt
@@ -2,12 +2,15 @@ package xyz.codingwithza.mystoryapp.view.register
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import xyz.codingwithza.mystoryapp.databinding.ActivityRegisterBinding
 import xyz.codingwithza.mystoryapp.util.Util
+import xyz.codingwithza.mystoryapp.view.ViewModelFactory
 
 class RegisterActivity : AppCompatActivity(), Util {
 
     private lateinit var binding: ActivityRegisterBinding
+    private lateinit var registerViewModel: RegisterViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +18,11 @@ class RegisterActivity : AppCompatActivity(), Util {
         setContentView(binding.root)
 
         setupFullScreenView(window, supportActionBar)
+        initViewModel()
+    }
+
+    private fun initViewModel() {
+        val factory: ViewModelFactory = ViewModelFactory.getInstance()
+        registerViewModel = ViewModelProvider(this, factory)[RegisterViewModel::class.java]
     }
 }
