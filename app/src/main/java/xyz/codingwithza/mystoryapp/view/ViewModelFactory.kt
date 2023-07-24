5ae66954746921f70ee63dc31460811a4d6ef666
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-24 14:24:11 +0700

    Register - Add Register View Model in View Model Factory

--- app/src/main/java/xyz/codingwithza/mystoryapp/view/ViewModelFactory.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/view/ViewModelFactory.kt
@@ -1,11 +1,21 @@
 package xyz.codingwithza.mystoryapp.view
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import xyz.codingwithza.mystoryapp.data.repository.AuthRepository
 import xyz.codingwithza.mystoryapp.di.Injection
+import xyz.codingwithza.mystoryapp.view.register.RegisterViewModel
 
 class ViewModelFactory private constructor(private val authRepository: AuthRepository) :
     ViewModelProvider.NewInstanceFactory() {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(RegisterViewModel::class.java)) {
+            return RegisterViewModel(authRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
+    }
 
     companion object {
         @Volatile
