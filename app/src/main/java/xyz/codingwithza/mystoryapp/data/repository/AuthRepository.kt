118c9a27dd66a68af38ea44d2c14f14fb06bb714
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-24 14:06:53 +0700

    Register - Add initial Register function in Auth Repository

--- app/src/main/java/xyz/codingwithza/mystoryapp/data/repository/AuthRepository.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/data/repository/AuthRepository.kt
@@ -1,13 +1,26 @@
 package xyz.codingwithza.mystoryapp.data.repository
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import retrofit2.Callback
+import xyz.codingwithza.mystoryapp.data.remote.Result
+import xyz.codingwithza.mystoryapp.data.remote.request.RegisterRequest
+import xyz.codingwithza.mystoryapp.data.remote.response.RegisterResponse
 import xyz.codingwithza.mystoryapp.data.remote.retrofit.ApiService
+
 
 class AuthRepository private constructor(
     private val apiService: ApiService
 ) {
 
-    fun registerUser() {
+    private val result = MediatorLiveData<Result<RegisterResponse>>()
 
+    fun registerUser(registerRequest: RegisterRequest): LiveData<Result<RegisterResponse>> {
+        result.value = Result.Loading
+        val client = apiService.register(registerRequest.name, registerRequest.email, registerRequest.password)
+        client
+            .enqueue(object : Callback<RegisterResponse> {})
+        return result
     }
 
     companion object {
