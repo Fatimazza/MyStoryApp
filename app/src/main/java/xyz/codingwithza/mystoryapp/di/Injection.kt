de4cc72519f03f40a5a3d98e714892cd2b28579c
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-24 14:20:17 +0700

    Data - Change Dependency Injection name for Auth Repository

--- app/src/main/java/xyz/codingwithza/mystoryapp/di/Injection.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/di/Injection.kt
@@ -4,7 +4,7 @@ import xyz.codingwithza.mystoryapp.data.repository.AuthRepository
 import xyz.codingwithza.mystoryapp.data.remote.retrofit.ApiConfig
 
 object Injection {
-    fun provideUserRepository(): AuthRepository {
+    fun provideAuthRepository(): AuthRepository {
         val apiService = ApiConfig.getApiService()
         return AuthRepository.getInstance(apiService)
     }
