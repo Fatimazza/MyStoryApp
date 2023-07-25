6ace8517df262bacdf5d35994558bc316db7f9fa
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-25 10:13:51 +0700

    Custom - Add custom Email Edit Text

---
+++ app/src/main/java/xyz/codingwithza/mystoryapp/view/custom/EmailEditText.kt
@@ -0,0 +1,15 @@
+package xyz.codingwithza.mystoryapp.view.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import com.google.android.material.textfield.TextInputEditText
+
+class EmailEditText: TextInputEditText {
+
+    constructor(context: Context) : super(context) {
+    }
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+    }
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+    }
+}
