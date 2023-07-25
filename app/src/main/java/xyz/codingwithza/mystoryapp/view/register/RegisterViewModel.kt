77a313405b165b6a8a4f10393891833f2e2cf3cb
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-25 10:51:08 +0700

    Register - Rename Auth Repository

--- app/src/main/java/xyz/codingwithza/mystoryapp/view/register/RegisterViewModel.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/view/register/RegisterViewModel.kt
@@ -5,8 +5,8 @@ import xyz.codingwithza.mystoryapp.data.remote.request.RegisterRequest
 import xyz.codingwithza.mystoryapp.data.repository.AuthRepository
 
 class RegisterViewModel(
-    private val registerRepository: AuthRepository
+    private val authRepository: AuthRepository
 ) : ViewModel() {
     fun registerUser(registerRequest: RegisterRequest) =
-        registerRepository.registerUser(registerRequest)
+        authRepository.registerUser(registerRequest)
 }
