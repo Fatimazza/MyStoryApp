28dc9705647474db6051e4cf85a279729217ec3a
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-25 10:56:57 +0700

    Login - Init View Model in Activity

--- app/src/main/java/xyz/codingwithza/mystoryapp/view/login/LoginActivity.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/view/login/LoginActivity.kt
@@ -2,12 +2,15 @@ package xyz.codingwithza.mystoryapp.view.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import xyz.codingwithza.mystoryapp.databinding.ActivityLoginBinding
 import xyz.codingwithza.mystoryapp.util.Util
+import xyz.codingwithza.mystoryapp.view.ViewModelFactory
 
 class LoginActivity : AppCompatActivity(), Util {
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var loginViewModel: LoginViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +18,11 @@ class LoginActivity : AppCompatActivity(), Util {
         setContentView(binding.root)
 
         setupFullScreenView(window, supportActionBar)
+        initViewModel()
+    }
+
+    private fun initViewModel() {
+        val factory: ViewModelFactory = ViewModelFactory.getInstance()
+        loginViewModel = ViewModelProvider(this, factory)[LoginViewModel::class.java]
     }
 }
