16ea06953c330985f715b8f4f820b63c9291a437
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-26 18:05:15 +0700

    Story - Bind Data in Story Adapter

--- app/src/main/java/xyz/codingwithza/mystoryapp/view/story/StoryAdapter.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/view/story/StoryAdapter.kt
@@ -1,24 +1,32 @@
 package xyz.codingwithza.mystoryapp.view.story
 
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import xyz.codingwithza.mystoryapp.data.local.ListStoryItem
 import xyz.codingwithza.mystoryapp.databinding.ItemStoryBinding
 
 class StoryAdapter(): RecyclerView.Adapter<StoryAdapter.StoryViewHolder>() {
 
+    private var storyList = ArrayList<ListStoryItem>()
+
     inner class StoryViewHolder (val binding: ItemStoryBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoryViewHolder {
-        TODO("Not yet implemented")
+        val binding = ItemStoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return StoryViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: StoryViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val stories = storyList[position]
+        holder.apply {
+            binding.apply {
+                tvStoryName.text = stories.name
+                tvStoryDesc.text = stories.description
+            }
+        }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
-
+    override fun getItemCount(): Int = storyList.size
 }
