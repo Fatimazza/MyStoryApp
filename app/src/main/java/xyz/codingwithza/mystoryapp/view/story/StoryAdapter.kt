195939da19488657feab6780ad10fc847bd1412e
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-26 18:00:01 +0700

    Story - Make Story Adapter extends its View Holder

--- app/src/main/java/xyz/codingwithza/mystoryapp/view/story/StoryAdapter.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/view/story/StoryAdapter.kt
@@ -4,7 +4,7 @@ package xyz.codingwithza.mystoryapp.view.story
 import androidx.recyclerview.widget.RecyclerView
 import xyz.codingwithza.mystoryapp.databinding.ItemStoryBinding
 
-class StoryAdapter() {
+class StoryAdapter(): RecyclerView.Adapter<StoryAdapter.StoryViewHolder>() {
 
     inner class StoryViewHolder (val binding: ItemStoryBinding) : RecyclerView.ViewHolder(binding.root)
 
