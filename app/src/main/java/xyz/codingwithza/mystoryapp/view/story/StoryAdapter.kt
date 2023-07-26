2f6b892122f8ca7a3bef4bdafe1e7e6e0872cb8c
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-26 18:00:48 +0700

    Story - Override needed Function in Story Adapter

--- app/src/main/java/xyz/codingwithza/mystoryapp/view/story/StoryAdapter.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/view/story/StoryAdapter.kt
@@ -1,11 +1,24 @@
 package xyz.codingwithza.mystoryapp.view.story
 
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import xyz.codingwithza.mystoryapp.databinding.ItemStoryBinding
 
 class StoryAdapter(): RecyclerView.Adapter<StoryAdapter.StoryViewHolder>() {
 
     inner class StoryViewHolder (val binding: ItemStoryBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoryViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: StoryViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 
 }
