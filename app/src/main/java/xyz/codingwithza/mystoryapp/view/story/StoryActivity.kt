605c08c292cf3958c392a9783f51a1a9b4ab8e2a
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-26 18:28:58 +0700

    Story - Implement View Binding

--- app/src/main/java/xyz/codingwithza/mystoryapp/view/story/StoryActivity.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/view/story/StoryActivity.kt
@@ -2,11 +2,14 @@ package xyz.codingwithza.mystoryapp.view.story
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import xyz.codingwithza.mystoryapp.R
+import xyz.codingwithza.mystoryapp.databinding.ActivityStoryBinding
+
+private lateinit var binding: ActivityStoryBinding
 
 class StoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_story)
+        binding = ActivityStoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
