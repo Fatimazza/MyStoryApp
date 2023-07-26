cd95a96fc86e13e3815aefa729430d342f7a7b6a
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-26 18:33:41 +0700

    Story - Show RecyclerView in Activity

--- app/src/main/java/xyz/codingwithza/mystoryapp/view/story/StoryActivity.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/view/story/StoryActivity.kt
@@ -2,6 +2,7 @@ package xyz.codingwithza.mystoryapp.view.story
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import xyz.codingwithza.mystoryapp.databinding.ActivityStoryBinding
 
 private lateinit var binding: ActivityStoryBinding
@@ -11,5 +12,16 @@ class StoryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityStoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupView()
+    }
+
+    private fun setupView() {
+        val storyAdapter = StoryAdapter()
+        binding.apply {
+            rvStory.adapter = storyAdapter
+            rvStory.layoutManager = LinearLayoutManager(this@StoryActivity)
+            rvStory.setHasFixedSize(true)
+        }
     }
 }
