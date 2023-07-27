da9a00690fc1bf3e68f179889c7e8c9f04185ed9
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-27 10:04:15 +0700

    Story - Add Get All Stories Error response in Story Repository

--- app/src/main/java/xyz/codingwithza/mystoryapp/data/repository/StoryRepository.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/data/repository/StoryRepository.kt
@@ -2,16 +2,18 @@ package xyz.codingwithza.mystoryapp.data.repository
 
 
 import androidx.lifecycle.MediatorLiveData
+import com.google.gson.Gson
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import xyz.codingwithza.mystoryapp.data.local.StoryResponse
 import xyz.codingwithza.mystoryapp.data.remote.Result
+import xyz.codingwithza.mystoryapp.data.remote.response.ErrorResponse
 import xyz.codingwithza.mystoryapp.data.remote.retrofit.ApiService
 
 class StoryRepository private constructor(
     private val apiService: ApiService
-){
+) {
 
     private val storyResult = MediatorLiveData<Result<StoryResponse>>()
 
@@ -27,8 +29,15 @@ class StoryRepository private constructor(
                         response.body()?.let {
                             storyResult.value = Result.Success(it)
                         }
+                    } else {
+                        val errorBody = Gson().fromJson(
+                            response.errorBody()?.charStream(),
+                            ErrorResponse::class.java
+                        )
+                        storyResult.value = Result.Error(errorBody.message.toString())
                     }
                 }
+
                 override fun onFailure(call: Call<StoryResponse>, t: Throwable) {
                     storyResult.value = Result.Error(t.message.toString())
                 }
