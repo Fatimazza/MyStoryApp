def0439026230ee1c8273a41512d790de733494a
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-27 10:18:42 +0700

    Story - Init View Model in Activity

--- app/src/main/java/xyz/codingwithza/mystoryapp/view/story/StoryActivity.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/view/story/StoryActivity.kt
@@ -2,10 +2,13 @@ package xyz.codingwithza.mystoryapp.view.story
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import xyz.codingwithza.mystoryapp.databinding.ActivityStoryBinding
+import xyz.codingwithza.mystoryapp.view.ViewModelFactory
 
 private lateinit var binding: ActivityStoryBinding
+private lateinit var storyViewModel: StoryViewModel
 
 class StoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +17,7 @@ class StoryActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setupView()
+        initViewModel()
     }
 
     private fun setupView() {
@@ -23,5 +27,10 @@ class StoryActivity : AppCompatActivity() {
             rvStory.layoutManager = LinearLayoutManager(this@StoryActivity)
             rvStory.setHasFixedSize(true)
         }
+    }
+
+    private fun initViewModel() {
+        val factory: ViewModelFactory = ViewModelFactory.getInstance(this)
+        storyViewModel = ViewModelProvider(this, factory)[StoryViewModel::class.java]
     }
 }
