4ea041fd192b891b53e03fb21a9e92da7b8230e4
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-27 10:11:29 +0700

    Story - Add Story View Model in View Model Factory

--- app/src/main/java/xyz/codingwithza/mystoryapp/view/ViewModelFactory.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/view/ViewModelFactory.kt
@@ -4,12 +4,15 @@ import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import xyz.codingwithza.mystoryapp.data.repository.AuthRepository
+import xyz.codingwithza.mystoryapp.data.repository.StoryRepository
 import xyz.codingwithza.mystoryapp.di.Injection
 import xyz.codingwithza.mystoryapp.view.login.LoginViewModel
 import xyz.codingwithza.mystoryapp.view.register.RegisterViewModel
+import xyz.codingwithza.mystoryapp.view.story.StoryViewModel
 
 class ViewModelFactory private constructor(
-    private val authRepository: AuthRepository
+    private val authRepository: AuthRepository,
+    private val storyRepository: StoryRepository
 ) : ViewModelProvider.NewInstanceFactory() {
 
     @Suppress("UNCHECKED_CAST")
@@ -21,6 +24,9 @@ class ViewModelFactory private constructor(
             modelClass.isAssignableFrom(LoginViewModel::class.java) -> {
                 LoginViewModel(authRepository) as T
             }
+            modelClass.isAssignableFrom(StoryViewModel::class.java) -> {
+                StoryViewModel(storyRepository) as T
+            }
             else -> throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
         }
     }
@@ -30,7 +36,9 @@ class ViewModelFactory private constructor(
         private var instance: ViewModelFactory? = null
         fun getInstance(context: Context): ViewModelFactory =
             instance ?: synchronized(this) {
-                instance ?: ViewModelFactory(Injection.provideAuthRepository(context))
+                instance ?: ViewModelFactory(
+                    Injection.provideAuthRepository(context),
+                    Injection.provideStoryRepository())
             }.also { instance = it }
     }
 }
