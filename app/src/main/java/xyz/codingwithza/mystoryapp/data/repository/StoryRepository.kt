6680d14438a35f23a6dce2662dbe0da67f471bf5
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-27 07:18:41 +0700

    Data - Add Dependency Injection for Story Repository

--- app/src/main/java/xyz/codingwithza/mystoryapp/data/repository/StoryRepository.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/data/repository/StoryRepository.kt
@@ -1,8 +1,25 @@
 package xyz.codingwithza.mystoryapp.data.repository
 
-class StoryRepository {
+
+import xyz.codingwithza.mystoryapp.data.remote.retrofit.ApiService
+
+class StoryRepository private constructor(
+    private val apiService: ApiService
+){
 
     fun getAllStories() {
 
+    }
+
+    companion object {
+        @Volatile
+        private var instance: StoryRepository? = null
+
+        fun getInstance(
+            apiService: ApiService
+        ): StoryRepository =
+            instance ?: synchronized(this) {
+                instance ?: StoryRepository(apiService)
+            }.also { instance = it }
     }
 }
--- app/src/main/java/xyz/codingwithza/mystoryapp/di/Injection.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/di/Injection.kt
@@ -7,13 +7,20 @@ import androidx.datastore.preferences.preferencesDataStore
 import xyz.codingwithza.mystoryapp.data.local.datastore.UserPreferences
 import xyz.codingwithza.mystoryapp.data.repository.AuthRepository
 import xyz.codingwithza.mystoryapp.data.remote.retrofit.ApiConfig
+import xyz.codingwithza.mystoryapp.data.repository.StoryRepository
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore("userPref")
 
 object Injection {
+
     fun provideAuthRepository(context: Context): AuthRepository {
         val preferences = UserPreferences.getInstance(context.dataStore)
         val apiService = ApiConfig.getApiService()
         return AuthRepository.getInstance(preferences, apiService)
+    }
+
+    fun provideStoryRepository(): StoryRepository {
+        val apiService = ApiConfig.getApiService()
+        return StoryRepository.getInstance(apiService)
     }
 }
