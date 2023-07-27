43eedca7ececb8c5fbc6c58a3b84f5e7f5509e2e
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-27 13:59:37 +0700

    Story Detail - Add new Activity

---
+++ app/src/main/java/xyz/codingwithza/mystoryapp/view/storydetail/StoryDetailActivity.kt
@@ -0,0 +1,12 @@
+package xyz.codingwithza.mystoryapp.view.storydetail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import xyz.codingwithza.mystoryapp.R
+
+class StoryDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_story_detail)
+    }
+}
