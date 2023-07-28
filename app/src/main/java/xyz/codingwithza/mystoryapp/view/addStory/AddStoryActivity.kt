8e7b85cbf34affe10b6c2d82b871032f8698f221
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-28 19:33:51 +0700

    Add - Open Intent Gallery to Add Story

--- app/src/main/java/xyz/codingwithza/mystoryapp/view/addStory/AddStoryActivity.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/view/addStory/AddStoryActivity.kt
@@ -1,8 +1,12 @@
 package xyz.codingwithza.mystoryapp.view.addStory
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import xyz.codingwithza.mystoryapp.databinding.ActivityAddStoryBinding
+import xyz.codingwithza.mystoryapp.util.uriToFile
 
 
 class AddStoryActivity : AppCompatActivity() {
@@ -19,7 +23,27 @@ class AddStoryActivity : AppCompatActivity() {
 
     private fun setupButtonAction() {
         binding.btnAddGallery.setOnClickListener {
+            openIntentGallery()
+        }
+    }
 
+    private fun openIntentGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg = result.data?.data as Uri
+            selectedImg.let { uri ->
+                val myFile = uriToFile(uri, this@AddStoryActivity)
+                binding.ivAddPreview.setImageURI(uri)
+            }
         }
     }
 }
