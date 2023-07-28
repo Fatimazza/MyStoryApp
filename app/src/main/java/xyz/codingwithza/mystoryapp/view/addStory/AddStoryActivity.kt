16cc9943ba3410c9ea169b6e134d31b2c9afd214
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-28 13:02:43 +0700

    Add - Add new Add Story activity

---
+++ app/src/main/java/xyz/codingwithza/mystoryapp/view/addStory/AddStoryActivity.kt
@@ -0,0 +1,12 @@
+package xyz.codingwithza.mystoryapp.view.addStory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import xyz.codingwithza.mystoryapp.R
+
+class AddStoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_story)
+    }
+}
