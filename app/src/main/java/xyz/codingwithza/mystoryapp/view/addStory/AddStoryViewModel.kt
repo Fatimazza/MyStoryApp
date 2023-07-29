79b49033a54bfb6b77fdb8f8384a1108d3ff89c3
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-29 08:58:04 +0700

    Add - Add Add Story View Model

--- app/src/main/java/xyz/codingwithza/mystoryapp/view/addStory/AddStoryViewModel.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/view/addStory/AddStoryViewModel.kt
@@ -1,10 +1,15 @@
 package xyz.codingwithza.mystoryapp.view.addStory
 
 import androidx.lifecycle.ViewModel
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import xyz.codingwithza.mystoryapp.data.repository.StoryRepository
 
 class AddStoryViewModel(
     private val storyRepository: StoryRepository
 ) : ViewModel() {
 
+    fun uploadStory(token: String, imageMultipart: MultipartBody.Part, description: RequestBody) {
+        storyRepository.uploadStory(token, imageMultipart, description)
+    }
 }
