b957da64793d1e3c5a1b99257c2eed05372d8614
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-29 07:50:18 +0700

    Add - Open Intent Camera Full Size

--- app/src/main/java/xyz/codingwithza/mystoryapp/view/addStory/AddStoryActivity.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/view/addStory/AddStoryActivity.kt
@@ -3,7 +3,7 @@ package xyz.codingwithza.mystoryapp.view.addStory
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
@@ -12,14 +12,19 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import xyz.codingwithza.mystoryapp.R
 import xyz.codingwithza.mystoryapp.databinding.ActivityAddStoryBinding
+import xyz.codingwithza.mystoryapp.util.createCustomTempFile
+import xyz.codingwithza.mystoryapp.util.rotateFile
 import xyz.codingwithza.mystoryapp.util.uriToFile
+import java.io.File
 
 
 class AddStoryActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddStoryBinding
+    private lateinit var currentPhotoPath: String
 
     companion object {
         private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
@@ -76,15 +81,28 @@ class AddStoryActivity : AppCompatActivity() {
 
     private fun openIntentCamera() {
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        launcherIntentCamera.launch(intent)
+        intent.resolveActivity(packageManager)
+        createCustomTempFile(application).also {
+            val photoURI: Uri = FileProvider.getUriForFile(
+                this@AddStoryActivity,
+                "xyz.codingwithza.mystoryapp",
+                it
+            )
+            currentPhotoPath = it.absolutePath
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            launcherIntentCamera.launch(intent)
+        }
     }
 
     private val launcherIntentCamera = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) {
         if (it.resultCode == RESULT_OK) {
-            val imageBitmap = it.data?.extras?.get("data") as Bitmap
-            binding.ivAddPreview.setImageBitmap(imageBitmap)
+            val myFile = File(currentPhotoPath)
+            myFile.let { file ->
+                rotateFile(file)
+                binding.ivAddPreview.setImageBitmap(BitmapFactory.decodeFile(file.path))
+            }
         }
     }
 
