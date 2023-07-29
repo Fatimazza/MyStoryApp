7cd7384b0a18277c41bc530cb9eea707191af1c7
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-29 08:26:16 +0700

    Add - Add Add Story Response model and API Service

--- app/src/main/java/xyz/codingwithza/mystoryapp/data/remote/retrofit/ApiService.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/data/remote/retrofit/ApiService.kt
@@ -1,8 +1,11 @@
 package xyz.codingwithza.mystoryapp.data.remote.retrofit
 
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.*
 import xyz.codingwithza.mystoryapp.data.local.StoryResponse
+import xyz.codingwithza.mystoryapp.data.remote.response.AddStoryResponse
 import xyz.codingwithza.mystoryapp.data.remote.response.LoginResponse
 import xyz.codingwithza.mystoryapp.data.remote.response.RegisterResponse
 
@@ -27,4 +30,11 @@ interface ApiService {
     fun getAllStory(
         @Header("Authorization") token: String
     ): Call<StoryResponse>
+
+    @Multipart
+    @POST("/v1/stories/guest")
+    fun uploadImage(
+        @Part file: MultipartBody.Part,
+        @Part("description") description: RequestBody,
+    ): Call<AddStoryResponse>
 }
