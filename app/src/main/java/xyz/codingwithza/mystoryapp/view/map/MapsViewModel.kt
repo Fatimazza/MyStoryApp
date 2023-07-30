7078d5a4e7581dd33691452c8034e104bb0dd24c
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-30 11:52:15 +0700

    Maps - Add Get Story function in View Model

--- app/src/main/java/xyz/codingwithza/mystoryapp/view/map/MapsViewModel.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/view/map/MapsViewModel.kt
@@ -1,10 +1,18 @@
 package xyz.codingwithza.mystoryapp.view.map
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import xyz.codingwithza.mystoryapp.data.local.datastore.UserModel
 import xyz.codingwithza.mystoryapp.data.repository.StoryRepository
 
 class MapsViewModel(
     private val storyRepository: StoryRepository
 ) : ViewModel() {
 
+    fun getAllStoriesByLocation(token: String) =
+        storyRepository.getAllStoriesByLocation(token)
+
+    fun getUserData(): LiveData<UserModel> {
+        return storyRepository.getUserData()
+    }
 }
