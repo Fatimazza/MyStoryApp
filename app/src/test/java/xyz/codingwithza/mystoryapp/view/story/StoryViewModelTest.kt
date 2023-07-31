1e2a24fce12b2d29490332f712355c4db8127731
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-31 19:11:02 +0700

    Testing - Test Paging Data in Adapter using Async Paging Data Differ

--- app/src/test/java/xyz/codingwithza/mystoryapp/view/story/StoryViewModelTest.kt
+++ app/src/test/java/xyz/codingwithza/mystoryapp/view/story/StoryViewModelTest.kt
@@ -3,11 +3,15 @@ package xyz.codingwithza.mystoryapp.view.story
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.paging.AsyncPagingDataDiffer
 import androidx.paging.PagingData
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
+import androidx.recyclerview.widget.ListUpdateCallback
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
+import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -44,7 +48,25 @@ class StoryViewModelTest {
         val storyViewModel = StoryViewModel(storyRepository)
         val actualStory: PagingData<ListStoryItem> =
             storyViewModel.getAllStories().getOrAwaitValue()
+
+        val differ = AsyncPagingDataDiffer(
+            diffCallback = StoryAdapter.DIFF_CALLBACK,
+            updateCallback = noopListUpdateCallback,
+            workerDispatcher = Dispatchers.Main,
+        )
+        differ.submitData(actualStory)
+
+        Assert.assertNotNull(differ.snapshot())
+        Assert.assertEquals(dummyStory.size, differ.snapshot().size)
+        Assert.assertEquals(dummyStory[0], differ.snapshot()[0])
     }
+}
+
+val noopListUpdateCallback = object : ListUpdateCallback {
+    override fun onInserted(position: Int, count: Int) {}
+    override fun onRemoved(position: Int, count: Int) {}
+    override fun onMoved(fromPosition: Int, toPosition: Int) {}
+    override fun onChanged(position: Int, count: Int, payload: Any?) {}
 }
 
 class StoryPagingSource : PagingSource<Int, LiveData<List<ListStoryItem>>>() {
