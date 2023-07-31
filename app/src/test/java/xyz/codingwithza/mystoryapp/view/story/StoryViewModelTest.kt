dfafa64ad373daa75c4a4c1a7e8088056ebc647d
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-31 19:03:27 +0700

    Testing - Add Story Paging Source in Testing Class

--- app/src/test/java/xyz/codingwithza/mystoryapp/view/story/StoryViewModelTest.kt
+++ app/src/test/java/xyz/codingwithza/mystoryapp/view/story/StoryViewModelTest.kt
@@ -1,12 +1,17 @@
 package xyz.codingwithza.mystoryapp.view.story
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
+import androidx.paging.PagingData
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Rule
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 import xyz.codingwithza.mystoryapp.MainDispatcherRule
+import xyz.codingwithza.mystoryapp.data.remote.response.ListStoryItem
 import xyz.codingwithza.mystoryapp.data.repository.StoryRepository
 
 @ExperimentalCoroutinesApi
@@ -24,3 +29,18 @@ class StoryViewModelTest {
     
 }
 
+class StoryPagingSource : PagingSource<Int, LiveData<List<ListStoryItem>>>() {
+    companion object {
+        fun snapshot(items: List<ListStoryItem>): PagingData<ListStoryItem> {
+            return PagingData.from(items)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, LiveData<List<ListStoryItem>>>): Int {
+        return 0
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, LiveData<List<ListStoryItem>>> {
+        return LoadResult.Page(emptyList(), 0, 1)
+    }
+}
