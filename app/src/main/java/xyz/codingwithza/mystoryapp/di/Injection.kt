8904912443c2649ffdd79d3cac2dfdd791ac41e4
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-31 21:23:24 +0700

    Network - Remove token Authorization as Interceptor in API Service

--- app/src/main/java/xyz/codingwithza/mystoryapp/di/Injection.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/di/Injection.kt
@@ -17,15 +17,13 @@ object Injection {
 
     fun provideAuthRepository(context: Context): AuthRepository {
         val preferences = UserPreferences.getInstance(context.dataStore)
-        val user = runBlocking { preferences.getUser().first() }
-        val apiService = ApiConfig.getApiService(user.token)
+        val apiService = ApiConfig.getApiService()
         return AuthRepository.getInstance(preferences, apiService)
     }
 
     fun provideStoryRepository(context: Context): StoryRepository {
         val preferences = UserPreferences.getInstance(context.dataStore)
-        val user = runBlocking { preferences.getUser().first() }
-        val apiService = ApiConfig.getApiService(user.token)
+        val apiService = ApiConfig.getApiService()
         return StoryRepository.getInstance(preferences, apiService)
     }
 }
