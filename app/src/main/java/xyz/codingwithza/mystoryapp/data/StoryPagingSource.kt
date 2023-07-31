3ea488d39811de0e627d7be9776798c214b2e14f
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-31 12:56:10 +0700

    Story - Get Refresh Key in Story Paging Source

--- app/src/main/java/xyz/codingwithza/mystoryapp/data/StoryPagingSource.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/data/StoryPagingSource.kt
@@ -5,13 +5,16 @@ import androidx.paging.PagingState
 import xyz.codingwithza.mystoryapp.data.remote.response.ListStoryItem
 
 class StoryPagingSource() : PagingSource<Int, ListStoryItem>() {
-    
-    override fun getRefreshKey(state: PagingState<Int, ListStoryItem>): Int? {
-        TODO("Not yet implemented")
-    }
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, ListStoryItem> {
         TODO("Not yet implemented")
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, ListStoryItem>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
     }
 
 }
