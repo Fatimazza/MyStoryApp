39debe7cc76c17caa4dd03cca7808019e76d603a
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-31 19:07:20 +0700

    Testing - Add Mock mechanism when calling Story data as Paging Data

--- app/src/test/java/xyz/codingwithza/mystoryapp/view/story/StoryViewModelTest.kt
+++ app/src/test/java/xyz/codingwithza/mystoryapp/view/story/StoryViewModelTest.kt
@@ -2,17 +2,23 @@ package xyz.codingwithza.mystoryapp.view.story
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.paging.PagingData
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito
 import org.mockito.junit.MockitoJUnitRunner
+import xyz.codingwithza.mystoryapp.DataDummy
 import xyz.codingwithza.mystoryapp.MainDispatcherRule
 import xyz.codingwithza.mystoryapp.data.remote.response.ListStoryItem
 import xyz.codingwithza.mystoryapp.data.repository.StoryRepository
+import xyz.codingwithza.mystoryapp.getOrAwaitValue
 
 @ExperimentalCoroutinesApi
 @RunWith(MockitoJUnitRunner::class)
@@ -27,6 +33,18 @@ class StoryViewModelTest {
     @Mock
     private lateinit var storyRepository: StoryRepository
     
+    @Test
+    fun `when Get Story Should Not Null and Return Data`() = runTest {
+        val dummyStory = DataDummy.generateDummyStoryResponse()
+        val data: PagingData<ListStoryItem> = StoryPagingSource.snapshot(dummyStory)
+        val expectedStory = MutableLiveData<PagingData<ListStoryItem>>()
+        expectedStory.value = data
+
+        Mockito.`when`(storyRepository.getAllStories()).thenReturn(expectedStory)
+        val storyViewModel = StoryViewModel(storyRepository)
+        val actualStory: PagingData<ListStoryItem> =
+            storyViewModel.getAllStories().getOrAwaitValue()
+    }
 }
 
 class StoryPagingSource : PagingSource<Int, LiveData<List<ListStoryItem>>>() {
