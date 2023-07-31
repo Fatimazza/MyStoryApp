da4a7a8ea5b34e8ec8bd6c1353be9480add8d677
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-07-31 13:44:06 +0700

    Network - Reformat Api Config code

--- app/src/main/java/xyz/codingwithza/mystoryapp/data/remote/retrofit/ApiConfig.kt
+++ app/src/main/java/xyz/codingwithza/mystoryapp/data/remote/retrofit/ApiConfig.kt
@@ -7,21 +7,21 @@ import retrofit2.converter.gson.GsonConverterFactory
 import xyz.codingwithza.mystoryapp.BuildConfig
 
 object ApiConfig {
-        fun getApiService(): ApiService {
-            val loggingInterceptor = if(BuildConfig.DEBUG) {
-                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-            } else {
-                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
-            }
-            val client = OkHttpClient.Builder()
-                .addInterceptor(loggingInterceptor)
-                .build()
-
-            val retrofit = Retrofit.Builder()
-                .baseUrl(BuildConfig.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(client)
-                .build()
-            return retrofit.create(ApiService::class.java)
+    fun getApiService(): ApiService {
+        val loggingInterceptor = if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        } else {
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
         }
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+        return retrofit.create(ApiService::class.java)
+    }
 }
